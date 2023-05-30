077251e468d8327729c353e859cf1133b97ca253
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-05-30 16:16:07 +0300

    Adde avatar box and 2 details boxes

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,7 +1,8 @@
 import { Link, useParams } from "react-router-dom";
 import { getUser } from "./Helpers";
 import { useQuery } from "@tanstack/react-query";
-import { SimpleGrid, Box, Container } from "@chakra-ui/react";
+import { SimpleGrid, Box, Container, Stack } from "@chakra-ui/react";
+import { Avatar } from "@chakra-ui/react";
 
 const User = () => {
   const { id } = useParams();
@@ -31,10 +32,20 @@ const User = () => {
   console.log(user);
   return (
     <Container maxW={"7xl"} p={2}>
-      User : {id}
-      <br />
-      <SimpleGrid columns={{ base: 3, md: 2, sm: 1 }} spacing={5}>
-        <Box bg="tomato" height="80px"></Box>
+      <SimpleGrid columns={{ base: 3, md: 2, sm: 1 }} spacing={3}>
+        <Stack direction="column">
+          <Box bg={"blue.400"} w={"100%"} textAlign={"center"} p={2}>
+            <Avatar size={"xl"} />
+          </Box>
+          <Stack direction={"row"}>
+            <Box bg={"purple.400"} w={"100%"} textAlign={"center"}>
+              23
+            </Box>
+            <Box bg={"green.400"} w={"100%"} textAlign={"center"}>
+              24
+            </Box>
+          </Stack>
+        </Stack>
         <Box bg="tomato" height="80px"></Box>
         <Box bg="tomato" height="80px"></Box>
       </SimpleGrid>
