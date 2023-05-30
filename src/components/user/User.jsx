c229ffc0e9b862a5448e2f78410badcbb0f5ed43
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-05-30 08:13:34 +0300

    added user component base setup

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,7 +1,7 @@
 import { Link, useParams } from "react-router-dom";
-
 import { getUser } from "./Helpers";
 import { useQuery } from "@tanstack/react-query";
+import { SimpleGrid, Box, Container } from "@chakra-ui/react";
 
 const User = () => {
   const { id } = useParams();
@@ -30,11 +30,16 @@ const User = () => {
   }
   console.log(user);
   return (
-    <div>
+    <Container maxW={"7xl"} p={2}>
       User : {id}
       <br />
+      <SimpleGrid columns={{ base: 3, md: 2, sm: 1 }} spacing={5}>
+        <Box bg="tomato" height="80px"></Box>
+        <Box bg="tomato" height="80px"></Box>
+        <Box bg="tomato" height="80px"></Box>
+      </SimpleGrid>
       <Link to={"/"}>Home</Link>
-    </div>
+    </Container>
   );
 };
 
