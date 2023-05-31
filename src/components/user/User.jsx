38f3a8899419351a64b0029f2e810b3265121bf2
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-05-31 10:56:40 +0300

    added user basic and contact details

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -1,7 +1,7 @@
 import { Link, useParams } from "react-router-dom";
 import { getUser } from "./Helpers";
 import { useQuery } from "@tanstack/react-query";
-import { SimpleGrid, Box, Container, Stack } from "@chakra-ui/react";
+import { SimpleGrid, Box, Container, Stack, Text } from "@chakra-ui/react";
 import { Avatar } from "@chakra-ui/react";
 
 const User = () => {
@@ -31,23 +31,39 @@ const User = () => {
   }
   console.log(user);
   return (
-    <Container maxW={"7xl"} p={2}>
+    <Container maxW={"5xl"} p={2}>
       <SimpleGrid columns={{ base: 3, md: 2, sm: 1 }} spacing={3}>
         <Stack direction="column">
-          <Box bg={"blue.400"} w={"100%"} textAlign={"center"} p={2}>
-            <Avatar size={"xl"} />
+          <Box
+            w={"100%"}
+            textAlign={"center"}
+            p={2}
+            border={"1px"}
+            borderColor={"black"}
+          >
+            <Avatar size={"xl"} bg="teal.500" />
           </Box>
           <Stack direction={"row"}>
-            <Box bg={"purple.400"} w={"100%"} textAlign={"center"}>
-              23
+            <Box
+              bg={"red.400"}
+              w={{ base: "60%" }}
+              p={2}
+              border={"1px"}
+              borderColor="gray.200"
+            >
+              <Text>{user.id}</Text>
+              <Text>{user.name}</Text>
+              <Text>{user.username}</Text>
             </Box>
-            <Box bg={"green.400"} w={"100%"} textAlign={"center"}>
-              24
+            <Box bg={"green.400"} w={"100%"} p={2}>
+              <Text noOfLines={1}>{user.phone}</Text>
+              <Text>{user.website}</Text>
+              <Text>{user.email}</Text>
             </Box>
           </Stack>
         </Stack>
-        <Box bg="tomato" height="80px"></Box>
-        <Box bg="tomato" height="80px"></Box>
+        <Box bg="tomato" height="80px" w={"100%"}></Box>
+        <Box bg="tomato" height="80px" w={"100%"}></Box>
       </SimpleGrid>
       <Link to={"/"}>Home</Link>
     </Container>
