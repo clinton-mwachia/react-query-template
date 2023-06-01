f11e3bc24d43e248f3bd3d38b4d0a9cbc7b555b6
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-06-01 18:33:17 +0300

    update user details

--- src/components/user/Helpers.jsx
+++ src/components/user/Helpers.jsx
@@ -4,3 +4,8 @@ export const getUser = async (id) => {
   const { data } = await Axios.get(`/users/${id}`);
   return data;
 };
+
+export const updateUser = async ({ id, formData }) => {
+  const { data } = await Axios.put(`/users/${id}`, formData);
+  return data;
+};
