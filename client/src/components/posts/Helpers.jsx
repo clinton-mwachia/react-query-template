586c84c6ed26cf4fa2db02ccd2958d47ae8f11df
clinton moshe mwachia <clintonmwachia9@gmail.com>
2023-06-12 16:42:46 +0300

    add post helper functions

--- client/src/components/posts/Helpers.jsx
+++ client/src/components/posts/Helpers.jsx
@@ -4,3 +4,8 @@ export const getPosts = async ({ page, limit = 10 }) => {
   const { data } = await Axios.get(`/posts?page=${page}&limit=${limit}`);
   return data;
 };
+
+export const newPost = async ({ formData }) => {
+  const { data } = await Axios.post(`/posts/register`, formData);
+  return data;
+};
